bd7345a3c3a471e272742bffb3e207dcf54bc4e7
Doug Neiner <doug@dougneiner.com>
2024-06-02 21:35:28 -0400

    Start to add blog to site
    This will make this post public, but will likely tweak it more before actually sharing it and adding the posts to the home page.

---
+++ src/content/config.ts
@@ -0,0 +1,15 @@
+import { z, defineCollection } from 'astro:content'
+
+const postsCollection = defineCollection({
+    type: 'content',
+    schema: z.object({
+        title: z.string(),
+        pubDate: z.date(),
+        description: z.string(),
+        category: z.string()
+    })
+})
+
+export const collections = {
+    posts: postsCollection
+}
